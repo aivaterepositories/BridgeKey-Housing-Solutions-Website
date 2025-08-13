c9f3b34d05066ddae75d9eacccd653b7a5daefca
[dyad] <mark@aivate.net>
2025-08-13 10:59:55 +0800

    [dyad] Adjusted partner logo spacing - wrote 1 file(s)

--- src/components/PartnershipSection.tsx
+++ src/components/PartnershipSection.tsx
@@ -39,7 +39,10 @@ const PartnershipSection = () => {
       <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
         <ul className="flex items-center justify-start animate-infinite-scroll">
           {partners.map((partner, index) => (
-            <li key={index} className="w-64 flex-shrink-0 flex justify-center items-center">
+            <li key={index} className={cn(
+              "flex-shrink-0 flex justify-center items-center",
+              partner.name === 'Sedgwick' ? 'w-40' : 'w-64'
+            )}>
               <img
                 src={partner.logoUrl}
                 alt={`${partner.name} logo`}
@@ -50,7 +53,10 @@ const PartnershipSection = () => {
         </ul>
         <ul className="flex items-center justify-start animate-infinite-scroll" aria-hidden="true">
           {partners.map((partner, index) => (
-            <li key={index} className="w-64 flex-shrink-0 flex justify-center items-center">
+            <li key={index} className={cn(
+              "flex-shrink-0 flex justify-center items-center",
+              partner.name === 'Sedgwick' ? 'w-40' : 'w-64'
+            )}>
               <img
                 src={partner.logoUrl}
                 alt={`${partner.name} logo`}
